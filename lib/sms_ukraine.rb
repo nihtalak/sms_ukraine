8534fc627991f068897662a0afe6cb8430c7dd38
Vladimir Oberemok <nihtalak@gmail.com>
2013-06-06 08:06:54 -0700

    Done with API

--- lib/sms_ukraine.rb
+++ lib/sms_ukraine.rb
@@ -1,5 +1,42 @@
 require "sms_ukraine/version"
-
 module SmsUkraine
-  # Your code goes here...
+  class SmsSender
+    require "open-uri"
+    BASE_URI = "http://smsukraine.com.ua/api/http.php?"
+
+    def initialize(log, pass)
+      @params = {login: log, password: pass, version: "http"}
+    end
+
+    def balance
+      execute(command: "balance")
+    end
+
+    def send_sms(name, to, message)
+      execute(command: "send", from: name, to: to, message: message)
+    end
+
+    def status(id)
+      execute(command: "receive", id: id)
+    end
+
+    private
+    def execute(hash)
+      result = send_request(hash)    
+      raise result[:errors] if result[:errors]
+      result
+    end
+
+    def send_request(hash)
+      command = URI.encode_www_form(hash.merge(@params))
+      result = open(BASE_URI + command) { |f| f.read }
+
+      hash = {}
+      result.split("\n").each do |line|
+        key, val = line.split(":")
+        hash[key.to_sym] = val
+      end
+      hash
+    end
+  end
 end
---
+++ spec/sms_ukraine/sms_sender_spec.rb
@@ -0,0 +1,74 @@
+require "spec_helper"
+
+module SmsUkraine
+  PHONE = "log"
+  PASS  = "pass"
+  describe SmsSender do
+    let(:sender) { SmsSender.new(PHONE, PASS) }
+
+
+    shared_examples 'request to smsukraine' do
+      it "should send http request to http://smsukraine.com.ua/api/http.php" do
+        sender.should_receive(:open).with(/http:\/\/smsukraine.com.ua\/api\/http.php/)
+      end
+    end
+
+    describe "#balance" do
+      before { sender.stub(:open).and_return("balance:1.23\n") }
+      after { sender.balance }
+
+      it_behaves_like "request to smsukraine" 
+
+      it "should send command=balance" do
+        sender.should_receive(:open).with(/command=balance/)
+      end
+
+      it "should return 1.23 when balance is 1.23UAH" do
+        sender.balance[:balance].should eq('1.23')
+      end
+    end
+
+    describe "#send" do
+      before { sender.stub(:open).and_return("id:103796881\nsms_count:1\n") }
+      context "url" do
+        after { sender.send_sms("test", "380930149181", "test message") }
+
+        it_behaves_like "request to smsukraine"
+
+        it "should send command=send&from=name&to=phone&message=message" do
+          sender.should_receive(:open).with(/command=send&from=test&to=380930149181&message=test\+message/)
+        end
+      end
+
+      context "result" do
+        it "should return message id and sms count" do
+          smsinfo = sender.send_sms("test", "number", "test")
+          smsinfo[:id].should eq("103796881")
+          smsinfo[:sms_count].should eq("1")
+        end
+      end
+    end
+
+
+    describe "status" do
+      before { sender.stub(:open).and_return("status:Доставлено\nstatus_code:3\n")}
+      context "url" do
+        after { sender.status("12312") }
+
+        it_behaves_like "request to smsukraine"
+
+        it "should send command=receive&id=id" do
+          sender.should_receive(:open).with(/command=receive&id=12312/)
+        end
+      end
+
+      context "result" do
+        it "should return message id and sms count" do
+          smsinfo = sender.status("12312")
+          smsinfo[:status].should eq("Доставлено")
+          smsinfo[:status_code].should eq("3")
+        end
+      end
+    end
+  end
+end
